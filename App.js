2b5299640c0789efb9858e5c5dfcd42efef77a83
Arthur Ransolin <arthur12022005ransolin@gmail.com>
2025-09-12 20:59:07 -0300

    implementacao completa das operacoes CRUD na API do projeto

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ app.use(express .json());
 
 let produtos = [
     {id: 1, nome: 'Teclado Mecânico', preco: 450.00},
-    {id: 2, nome: 'MOuse razer', preco: 150.00},
+    {id: 2, nome: 'Mouse razer', preco: 150.00},
     {id: 3, nome: 'Monitor 144hz', preco: 1200.00},
 ];
 let nextId = 4;
@@ -19,8 +19,15 @@ app.get('/sobre', (req,res) => {
     res.send('Esta é a página "sobre" da nossa API');
 });
 
-app.get('/app/produtos', (req,res) =>{
-    res.json(produtos);
+app.get('/api/produtos/:id', (req, res) => {
+  const idProduto = parseInt(req.params.id);
+  const produtoEncontrado = produtos.find(p => p.id === idProduto);
+
+  if (produtoEncontrado) {
+    res.json(produtoEncontrado);
+  } else {
+    res.status(404).send('Produto não encontrado.');
+  }
 });
 
 app.post('/api/produtos', (req, res) => {
@@ -32,7 +39,7 @@ app.post('/api/produtos', (req, res) => {
     }
 
     const novoProduto = {
-        id: nextID++,
+        id: nextId++,
         nome,
         preco
     };
@@ -41,6 +48,45 @@ app.post('/api/produtos', (req, res) => {
 
     res.status(201).json(novoProduto);
 })
+
+app.put('/api/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const produtoIndex = produtos.findIndex(p => p.id === id);
+
+    if (produtoIndex !== -1){
+        const {nome, preco} = req.body;
+
+        if (!nome && preco === undefined){
+            return res.status(400).json({ message: 'Pelo menos um campo (nome ou preco) deve ser fornecido para atualização.'});
+        }
+    
+
+        produtos[produtoIndex] = {
+            ...produtos[produtoIndex],
+            nome: nome !== undefined ? nome : produtos[produtoIndex].nome,
+            preco: preco !== undefined ? preco : produtos[produtoIndex].preco,
+        };
+
+        res.json(produtos[produtoIndex]);
+    } else {
+        res.status(404).json({message: 'Produto não encontrado para atualização.'});
+    }
+
+});
+
+app.delete('/api/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const initialLength = produtos.length;
+
+    produtos = produtos.filter(p => p.id !== id);
+
+    if (produtos.length < initialLength){
+        res.status(204).send();
+    } else {
+        res.status(404).json({message: 'Produto não encontrado para exclusão.'});
+    }
+});
 
 
 app.listen(PORT, () => {
